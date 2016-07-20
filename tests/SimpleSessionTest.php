e1eaf2e2d8df78a0cb6058b2a5a3aeae94563857
Lars Moelleken <lmoelleken@menadwork.com>
2016-07-20 10:41:24 +0200

    [+]: use "assertSame" instead of "assertEquals"

--- tests/SimpleSessionTest.php
+++ tests/SimpleSessionTest.php
@@ -41,9 +41,9 @@ class SimpleSessionTest extends PHPUnit_Framework_TestCase
   {
     $settings = $this->session2DB->get_settings();
 
-    self::assertEquals('3600 seconds (60 minutes)', $settings['session.gc_maxlifetime']);
-    self::assertEquals('1', $settings['session.gc_probability']);
-    self::assertEquals('1000', $settings['session.gc_divisor']);
+    self::assertSame('3600 seconds (60 minutes)', $settings['session.gc_maxlifetime']);
+    self::assertSame('1', $settings['session.gc_probability']);
+    self::assertSame('1000', $settings['session.gc_divisor']);
     self::assertContains('0.1', $settings['probability']);
     self::assertContains('%', $settings['probability']);
   }
@@ -53,7 +53,7 @@ class SimpleSessionTest extends PHPUnit_Framework_TestCase
     $_SESSION['test'] = 123;
     $this->session2DB->write($this->session_id, serialize($_SESSION));
 
-    self::assertEquals('123', $_SESSION['test']);
+    self::assertSame(123, $_SESSION['test']);
   }
 
   public function testBasic2()
@@ -61,7 +61,7 @@ class SimpleSessionTest extends PHPUnit_Framework_TestCase
     $data = $this->session2DB->read($this->session_id);
     $_SESSION = unserialize($data);
 
-    self::assertEquals('123', $_SESSION['test']);
+    self::assertSame(123, $_SESSION['test']);
   }
 
   public function testDestroy()
@@ -70,21 +70,21 @@ class SimpleSessionTest extends PHPUnit_Framework_TestCase
     $this->session2DB->destroy($this->session_id);
     $sessionsCount2 = $this->session2DB->get_active_sessions();
 
-    self::assertEquals('1', $sessionsCount1);
-    self::assertEquals('0', $sessionsCount2);
-    self::assertEquals('0', count($_SESSION));
+    self::assertSame(1, $sessionsCount1);
+    self::assertSame(0, $sessionsCount2);
+    self::assertSame(0, count($_SESSION));
   }
 
   public function testLall()
   {
     $this->session2DB->set_flashdata('test2', 'lall');
-    self::assertEquals('lall', $_SESSION['test2']);
+    self::assertSame('lall', $_SESSION['test2']);
 
     $this->session2DB->_manage_flashdata();
-    self::assertEquals('lall', $_SESSION['test2']);
+    self::assertSame('lall', $_SESSION['test2']);
 
     $this->session2DB->_manage_flashdata();
-    self::assertEquals(false, isset($_SESSION['test2']));
+    self::assertSame(false, isset($_SESSION['test2']));
   }
 
   public function setUp()
