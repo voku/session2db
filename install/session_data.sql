b606ddb8525524a3c29c78438e76d038db3d4702
Lars Moelleken <lars@moelleken.org>
2017-05-04 11:46:55 +0200

    [+]: testing more future-proof sql-query (no code changes, so it's still no BC) v2

--- install/session_data.sql
+++ install/session_data.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `session_data` (
-  `session_id` varchar(256) NOT NULL default '',
-  `hash` varchar(256) NOT NULL default '',
+  `session_id` varchar(128) NOT NULL default '',
+  `hash` varchar(128) NOT NULL default '',
   `session_data` blob NOT NULL,
   `session_expire` int(11) NOT NULL default 0,
   PRIMARY KEY session_id (`session_id`),
   KEY hash (`hash`),
   KEY session_expire (`session_expire`),
   KEY select_helper_index (`session_id`, `hash`, `session_expire`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
