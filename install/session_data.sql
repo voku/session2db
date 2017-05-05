fef20bd6f7d1f8f156e2a533a619507a5a4e1175
Lars Moelleken <lars@moelleken.org>
2017-05-05 10:34:59 +0200

    [*]: try to fix travis-ci build v2.1

--- install/session_data.sql
+++ install/session_data.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `session_data` (
-  `session_id` varchar(128) NOT NULL default '',
-  `hash` varchar(128) NOT NULL default '',
-  `session_data` blob NOT NULL,
-  `session_expire` int(11) NOT NULL default 0,
+CREATE TABLE session_data (
+  session_id varchar(128) NOT NULL default "",
+  hash varchar(128) NOT NULL default "",
+  session_data blob NOT NULL,
+  session_expire int(11) NOT NULL default "0",
   PRIMARY KEY session_id (`session_id`),
   KEY hash (`hash`),
   KEY session_expire (`session_expire`),
